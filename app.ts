9b21be1090d1d575e8e6713c70a385054bf4d4b5
Gabriel Budke <gabrielbudke@gmail.com>
2019-05-05 15:37:30 -0300

    Initial commit

---
+++ app.ts
@@ -0,0 +1,14 @@
+// Declaração de varável tipo: string
+let message: string = "Help me, Obi-Wan Kenobi. You're my only hope!";
+console.log(message);
+
+// Declaração de variável tipo: number
+let episode: number = 4;
+console.log("This is episode " + 4);
+episode = episode + 1;
+console.log("Nex episode is " + episode);
+
+// Declaração de varável sem tipo
+let favoriteDroid: string
+favoriteDroid = 'BB-8';
+console.log("My favorite droid is " + favoriteDroid);
---
+++ dist/app.js
@@ -0,0 +1,12 @@
+// Declaração de varável tipo: string
+var message = "Help me, Obi-Wan Kenobi. You're my only hope!";
+console.log(message);
+// Declaração de variável tipo: number
+var episode = 4;
+console.log("This is episode " + 4);
+episode = episode + 1;
+console.log("Nex episode is " + episode);
+// Declaração de varável sem tipo
+var favoriteDroid;
+favoriteDroid = 'BB-8';
+console.log("My favorite droid is " + favoriteDroid);
