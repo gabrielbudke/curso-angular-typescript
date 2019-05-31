360cb46f91d5aae1112604e1491b0e7a8836e142
Gabriel Budke <gabrielbudke@gmail.com>
2019-05-30 21:19:36 -0300

    Angular e Typescript

--- classe-main.ts
+++ classe-main.ts
@@ -1,9 +1,10 @@
+// Importando os módulos que contém as classes e interfaces
 import { Spacecraft, ContainerShip } from './base-ships'
 import { MilleniumFalcon } from './starfighter'
 
+// Importando e usando a biblioteca 'lodash'
 import * as _ from 'lodash'
 console.log(_.pad('Typescript Examples', 40, '='))
-
 
 // Instanciando a classe
 let ship = new Spacecraft('hyperdrive')
